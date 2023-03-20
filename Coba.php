62d35e0ebfe6939fbcd270ccba35b9b2a63e40ad
Tegar11-F <81769115+Tegar11-F@users.noreply.github.com>
2023-03-20 16:49:21 +0700

    baru

---
+++ Coba.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\M_infak;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        echo "kelas Coba";
+    }
+    public function coba1($nama)
+    {
+        // return view('welcome_message');
+
+        echo "kelas Coba1 selamat datang ".$nama;
+    }
+    public function testdatabase()
+    {
+        $model = model("M_infak");
+        // $this->M_infak = model("M_infak");
+        $query = $model->getdata();
+        $data = array(
+            'query' => $query,
+            'judul'=> "data infak"
+        );
+        return view('v_coba',$data);
+    }
+}
